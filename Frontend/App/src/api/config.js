821630bf89f67dff07dd05bb1759c0c4eaeb05e6
Kaashif Ameen <Kaashifameen32@gmail.com>
2025-05-26 17:14:37 +0300

    9

--- Frontend/App/src/api/config.js
+++ Frontend/App/src/api/config.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'https://aurora-vtm6.onrender.com',
-    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
-    }
+        'Accept': 'application/json',
+    },
+    withCredentials: false,
 });
 
-export default api; 
+export default api;
