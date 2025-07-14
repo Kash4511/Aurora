a7e5b6504f7b4e97e4b5dc7fe5ff6bd017a46d70
Kaashif Ameen <Kaashifameen32@gmail.com>
2025-07-14 15:09:33 +0530

    .

--- Backend/aurora/asgi.py
+++ Backend/aurora/asgi.py
@@ -1,23 +1,16 @@
 import os
-import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-import chat.routing  
-from chat.middleware import JWTAuthMiddleware
-
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aurora.settings')
 
-
+import django
 django.setup()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat.routing
+from chat.middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter({
-    
     "http": get_asgi_application(),
-
-   
     "websocket": JWTAuthMiddleware(
         URLRouter(
             chat.routing.websocket_urlpatterns
